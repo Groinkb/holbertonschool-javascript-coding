ed239b341831527ef81c2418a6b9f0ba82b5e565
Groinkb <benjamin@mossu.ch>
2024-01-26 11:11:37 +0100

    add

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
-// Write a script that prints the title of a Star Wars movie where the
-// episode number matches a given integer.
-
+//write
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-const id = process.argv[2];
-const endpoint = `https://swapi-api.hbtn.io/api/films/${id}/`;
-
-request.get(endpoint, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Unexpected status code:', response.statusCode);
-    } else {
-        const movieData = JSON.parse(body);
-        console.log(movieData.title);
+request(url, (err, response, body) => {
+    if (!err) {
+        const data = JSON.parse(body);
+        console.log(data.title);
     }
 });
