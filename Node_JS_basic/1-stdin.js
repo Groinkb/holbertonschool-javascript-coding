779e994657aa4940bdeec91bf4af1d7e96b062f2
Groinkb <benjamin@mossu.ch>
2024-02-02 13:16:53 +0100

    add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
 const readline = require('readline');
 
 const readInput = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
 readInput.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
-    console.log('This important software is now closing');
-    readInput.close();
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  readInput.close();
 });
 
 module.exports = { readInput };
--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 const port = 1245;
 const hostname = '127.0.0.1';
 
 app.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}/`);
+  console.log(`Server is running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
