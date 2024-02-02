2aef8a226cd3ee8ccc239a065280034aaf4bca7d
Groinkb <benjamin@mossu.ch>
2024-02-02 13:11:09 +0100

    add

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (error, data) => {
+            if (error) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+
+            const students = data.split('\n').filter(Boolean).slice(1).map((line) => {
+                const [firstname, lastname, age, field] = line.split(',');
+                return {
+                    firstname, lastname, age, field,
+                };
+            });
+
+            const csStudents = students
+                .filter((student) => student.field === 'CS')
+                .map((student) => student.firstname).join(', ');
+
+            const sweStudents = students
+                .filter((student) => student.field === 'SWE')
+                .map((student) => student.firstname).join(', ');
+
+            console.log(`Number of students: ${students.length}`);
+            console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents}`);
+            console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents}`);
+
+            // Résoudre la promesse avec les données des étudiants
+            resolve({
+                total: students.length,
+                cs: csStudents,
+                swe: sweStudents,
+            });
+        });
+    });
+}
+
+module.exports = countStudents;
